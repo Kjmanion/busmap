793d8611e0e19116e24cf2ce4640c2017bf9cfce
Kevin M <kman17@gmail.com>
2016-05-08 10:08:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JpZWdpdGUiLCJhIjoiN09DU0VUMCJ9.xog8FYRRF4rbv6Y0bFMvDA';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/griegite/cinxa7dta002pb1mavrbqmvm7',
+    zoom:10.5,
+    minZoom:9,
+    maxZoom:20,
+      
+  });
+
+map.setCenter([-76.531485,38.986766])
+
